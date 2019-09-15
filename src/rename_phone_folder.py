11fc61a673875b20eab001000223e6c66d8cb37f
Toby <tianer2820@163.com>
2019-09-15 21:58:06 +0800

    add some scripts

---
+++ src/rename_phone_folder.py
@@ -0,0 +1,29 @@
+import os
+import re
+
+cwd = os.getcwd()
+file_list = os.listdir(cwd)
+print(file_list)
+for i in file_list:
+    if os.path.isdir(cwd + os.path.sep + i):
+        for c in i:
+            if c not in '1234567890-_ ':
+                break
+        else:
+            result = re.match('([1234567890]{4})[-_ ]([1234567890]+)[-_ ]?([1234567890]+)?', i)
+            if result:
+                print(result.groups())
+                if len(result.group(2)) == 1:
+                    new_name = result.group(1) + '-' + '0' + result.group(2)
+                else:
+                    new_name = result.group(1) + '-' + result.group(2)
+                if result.group(3):
+                    if len(result.group(3)) == 1:
+                        new_name += '-' + '0' + result.group(3)
+                    else:
+                        new_name += '-' + result.group(3)
+                if os.path.exists(cwd + os.path.sep + new_name):
+                    print(new_name, 'already exists!')
+                else:
+                    os.rename(cwd + os.path.sep + i, cwd + os.path.sep + new_name)
+                    print('successfully renamed', new_name)
---
+++ src/video_in_folders.py
@@ -0,0 +1,16 @@
+import os
+
+cwd = os.getcwd()
+file_list = os.listdir(cwd)
+for f in file_list:
+    if os.path.isfile(cwd + os.path.sep + f):
+        name = f.split('_')
+        if name[0] != 'video':
+            continue
+        else:
+            date = name[1]
+            year = date[0:4]
+            month = date[4:6]
+            if not os.path.exists(cwd + os.path.sep + year + '-' + month):
+                os.mkdir(cwd + os.path.sep + year + '-' + month + os.path.sep)
+            os.rename(cwd + os.path.sep + f, cwd + os.path.sep + year + '-' + month + os.path.sep + f)
